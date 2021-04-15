2f697dc5b220b5e7e42717eae52267c0f37bb0ec
Dallin Moak <dallinmoak@gmail.com>
2021-04-15 10:31:02 -0600

    added basic routing and navbar with basic styling

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-export default class App extends Component {
-  constructor(){
-    super();
-  }
-  
-  render() {
-    return(
-      <div className='main-content'>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+import Home from './home.jsx';
+import About from './about.jsx';
+
+export default function App() {
+  return(
+    <Router>
+        <main>
+        <nav className="nav-main">
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+        </nav>
+        <div className="main-content">
+          <div className="page-main">
+            <Switch>
+              <Route path="/" exact>
+                <h1>Home</h1>
+                <Home />
+              </Route>
+              <Route path="/About">
+              <h1>About</h1>
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </main>
+    </Router>
+  );
 }
