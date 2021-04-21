145e7ffc29d516de16cf028f888e1044142ca0c5
Dallin Moak <dallinmoak@gmail.com>
2021-04-20 18:15:34 -0600

    remove class from portfolio-list component

--- src/components/portfolio-list.jsx
+++ src/components/portfolio-list.jsx
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import { portfolio } from '../../static/variables.js';
 let [ baseURL, listPath, listResponse ] = 
 [portfolio.baseURL, portfolio.listPath, portfolio.listResponse];
 
-export default class PortfolioList extends Component {
-  constructor(){
-    super();
-    this.state = {
-      portfolioList: []
-    }
-    this.getPortfolioList = this.getPortfolioList.bind(this);
-  }
+export default function PortfolioList(){
+  const [ portfolioList, setPortfolioList ] = useState([]);
 
-  getPortfolioList() {
+  function getPortfolioList() {
     axios.get( baseURL + listPath, { withCredentials: true })
     .then(res => {
       let pList = eval('res.' + listResponse);
-      this.setState({
-        portfolioList: pList
-      })    
+      setPortfolioList(pList);
     })
   }
 
-  printPortfolioList(){
-    let pList = this.state.portfolioList;
+  function printPortfolioList(){
+    let pList = portfolioList;
     if(pList){
       return pList.map( item => {
         return(<div key={item.id}>{item.name}</div>)
       })
     }
   }
-
-  componentDidMount(){
-    this.getPortfolioList();
-  }
   
-  render() {
-    return(
-      <div>
-        {this.printPortfolioList()}
-      </div>
-    );
-  }
+  useEffect(() => {
+    getPortfolioList();
+  })
+
+  return (
+    <div>
+      {printPortfolioList()}
+    </div>
+  );
 }
